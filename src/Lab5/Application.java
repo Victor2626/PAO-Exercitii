55d34178e28971cb2a7123f1a10051ae340ad93a
Victor2626 <119161177+Victor2626@users.noreply.github.com>
2024-03-26 15:19:03 +0200

    Add files via upload

---
+++ src/Lab5/Application.java
@@ -0,0 +1,40 @@
+package Lab5;
+
+import java.util.regex.*;
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args)
+    {
+        String text = "Odata creat un sir de caractere cu continutul sau nu mai poate fi modificat.";
+
+        System.out.println("1. Lungimea stringului: " + text.length());
+
+        boolean onlyLetters = text.matches("[a-zA-Z]+");
+        System.out.println("2. Contine doar litere: " + onlyLetters);
+
+        String[] words = text.split("\\s+");
+        int middleIndex = words.length / 2;
+        String middleWords = words[middleIndex];
+        if (words.length % 2 == 0) {
+            middleWords += " " + words[middleIndex - 1];
+        }
+        System.out.println("3. Cuvintele de la mijloc: " + middleWords);
+
+        StringBuilder reversedText = new StringBuilder(text).reverse();
+        System.out.println("4. Stringul inversat: " + reversedText);
+
+        String ex1="c",ex2="x",ex3="a",ex4="B",ex5="aB",ex6="baccbacb",ex7="",ex8="abcbc",ex9="cbabcabcaacbbcccabca",ex10="bbbcacab";
+        System.out.println(ex1.matches("[abc]"));
+        System.out.println(ex2.matches("[^abc]"));
+        System.out.println(ex3.matches("[a-z]"));
+        System.out.println(ex4.matches("[a-zA-Z]"));
+        System.out.println(ex5.matches("[a-z][A-Z]"));
+        System.out.println(ex6.matches("[abc]+"));;
+        System.out.println(ex7.matches("[abc]*"));
+        System.out.println(ex8.matches("[abc]{5}"));
+        System.out.println(ex9.matches("[abc]{5,}"));
+        System.out.println(ex10.matches("[abc]{5,10}"));
+
+    }
+}
